ae8100a25b5cbd15bd7dbb1e0eea51f2a6175e23
Felix Hauer <hauerf98@gmail.com>
2019-05-26 15:27:37 +0200

    finished chapter 3

--- 3_data_trafo_dplyr/summarize_pipes.r
+++ 3_data_trafo_dplyr/summarize_pipes.r
@@ -1,6 +1,13 @@
 library(nycflights13)
 library(tidyverse)
 
+flights_sml <- select(flights, 
+                      year:day,
+                      ends_with("delay"),
+                      distance,
+                      air_time
+)
+
 summarize(flights, delay = mean(dep_delay, na.rm = TRUE))
 
 by_day <- group_by(flights, year, month, day)
@@ -23,3 +30,104 @@ delays <- flights %>%
             dist = mean(distance, na.rm = TRUE),
             delay = mean(arr_delay, na.rm = TRUE)) %>% 
   filter(count > 20, dest != "HNL")
+
+#missing values
+flights %>%
+  group_by(year, month, day) %>%
+  summarise(mean = mean(dep_delay))
+
+not_cancelled <- flights %>%
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>%
+  group_by(year, month, day) %>%
+  summarize(mean = mean(dep_delay))
+
+#count
+delays <- not_cancelled %>%
+  group_by(tailnum) %>%
+  summarise(delay = mean(arr_delay))
+
+ggplot(data = delays, mapping = aes(x = delay)) +
+  geom_freqpoly(binwidth = 10)
+
+delays <- not_cancelled %>%
+  group_by(tailnum) %>%
+  summarise(delay = mean(arr_delay, na.rm = TRUE), n = n())
+
+ggplot(data = delays, mapping = aes(x = n, y = delay)) +
+  geom_point(alpha = 1 / 10)
+
+delays %>%
+  filter(n > 25) %>%
+  ggplot(mapping = aes(x = n, y = delay)) +
+  geom_point(alpha = 1 / 10)
+  
+#baseball data
+#install.packages(Lahman)
+#library(Lahman)
+#batting <- as_tibble(Lahman::Batting)
+
+
+#summary functions
+not_cancelled %>% 
+  group_by(year, month, day) %>%
+  summarize(
+    avg_delay1 = mean(arr_delay),
+    avg_delay2 = mean(arr_delay[arr_delay > 0])
+  )
+
+not_cancelled %>%
+  group_by(dest) %>%
+  summarize(distance_sd = sd(distance)) %>%
+  arrange(desc(distance_sd))
+
+not_cancelled %>%
+  group_by(year, month, day) %>%
+  mutate(r = min_rank(desc(dep_time))) %>%
+  filter(r %in% range(r))
+
+not_cancelled %>%
+  group_by(dest) %>%
+  summarize(carriers = n_distinct(carrier)) %>%
+  arrange(desc(carriers))
+
+not_cancelled %>%
+  count(dest)
+
+not_cancelled %>%
+  count(tailnum, wt = distance)
+
+not_cancelled %>%
+  group_by(year, month, day) %>%
+  summarize(n_early = sum(dep_time < 500))
+
+not_cancelled %>%
+  group_by(year, month, day) %>%
+  summarize(hour_perc = mean(arr_delay > 60))
+
+#grouping by multiple variables, progressively rolling up
+daily <- group_by(flights, year, month, day)
+(per_day <- summarize(daily, flights = n()))
+(per_month <- summarize(per_day, flights = sum(flights)))
+(per_year <- summarize(per_month, flights = sum(flights)))
+
+#ungrouping
+daily %>%
+  ungroup() %>%
+  summarize(flight=n())
+
+#grouped mutates (and filters)
+flights_sml %>%
+  group_by(year, month, day) %>%
+  filter(rank(desc(arr_delay)) < 10)
+ 
+popular_dests <- flights %>%
+  group_by(dest) %>%
+  filter(n() > 365)
+popular_dests 
+
+popular_dests %>%
+  filter(arr_delay > 0) %>%
+  mutate(prop_delay = arr_delay / sum(arr_delay)) %>%
+  select(year:day, dest, arr_delay, prop_delay)
